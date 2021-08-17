a6a8819ecbe4de5c4fb4256001299aaf88f0c586
Nathan Berthier <nathanberthier@outlook.fr>
2021-08-17 13:53:57 +0200

    Creation page show

--- app/controllers/organs_controller.rb
+++ app/controllers/organs_controller.rb
@@ -2,4 +2,10 @@ class OrgansController < ApplicationController
   def index
     @organs = Organ.all
   end
+
+  def show
+    @organ = Organ.find(params[:id])
+    @owner = User.find(@organ.owner_id)
+    @booking = Booking.new
+  end
 end
