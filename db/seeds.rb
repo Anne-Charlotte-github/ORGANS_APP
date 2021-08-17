91b459d84fd8f7378bb69c60f35642800c25642d
Nathan Berthier <nathanberthier@outlook.fr>
2021-08-17 12:46:40 +0200

    Seed for organs and users

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,17 +5,25 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-require 'faker'
-User.destroy_all
 Organ.destroy_all
+User.destroy_all
 puts 'Clear db'
+
+organ_type = ['poumon', 'coeur', 'foie', 'pancréas']
+
+user = User.new(email: 'toto@gmail.com', password: '123456', password_confirmation: '123456')
+user.save!
+organ = Organ.new(organ_type: organ_type.sample, city: 'Bourg en Bresse', condition: 'Pas ouf', price: 20.0)
+organ.owner_id = user.id
+organ.save!
+puts "Add user #{user.email}"
 
 10.times do
   user = User.new(email: Faker::Internet.email, password: 'secret_password', password_confirmation: 'secret_password')
   user.save!
-  organ = Organ.new(type: 'Poumon', city: 'Lyon', condition: 'Pas ouf', price: 20.0)
-  organ.owner_id = user
+  organ = Organ.new(organ_type: organ_type.sample, city: 'Lyon', condition: 'Pas ouf', price: 20.0)
+  organ.owner_id = user.id
   organ.save!
-  puts "Add user #{user.name}"
+  puts "Add user #{user.email}"
 end
 puts 'Finish'
