02aa2334579dd57346580d0a8e75137141aa61e9
Nathan Berthier <nathanberthier@outlook.fr>
2021-08-17 15:47:14 +0200

    Finish show page

--- app/controllers/organs_controller.rb
+++ app/controllers/organs_controller.rb
@@ -1,11 +1,11 @@
 class OrgansController < ApplicationController
+  skip_before_action :authenticate_user!, only: :index
   def index
     @organs = Organ.all
   end
 
   def show
     @organ = Organ.find(params[:id])
-    @owner = User.find(@organ.owner_id)
     @booking = Booking.new
   end
 end
