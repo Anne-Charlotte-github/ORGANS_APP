845f3a449c07ccb3bb3427b23107dc4c55e6e95a
Anne-Charlotte Morizot <annecharlotte.morizot@gmail.com>
2021-08-17 16:55:08 +0200

    creation de la branche

---
+++ app/controllers/bookings_controller.rb
@@ -0,0 +1,20 @@
+class BookingsController < ApplicationController
+  def create
+    @organ = Organ.find(params[:organ_id])
+    @booking = Booking.new(params_booking)
+    @booking.organ_id = params[:organ_id]
+    @booking.customer_id = current_user.id
+
+    if @booking.save
+      redirect_to user_path(current_user.id)
+    else
+      render("organs/show")
+    end
+  end
+
+  private
+
+  def params_booking
+    params.require(:booking).permit(:renting_at)
+  end
+end
