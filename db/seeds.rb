aec80cfdadd18ed8cd163dcc6e84d16b17d6563f
Anne-Charlotte Morizot <annecharlotte.morizot@gmail.com>
2021-08-17 12:47:42 +0200

    save to pull le master, et recup les seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Organ.destroy_all
+User.destroy_all
+puts 'Clear db'
+
+organ_type = ['poumon', 'coeur', 'foie', 'pancréas']
+
+10.times do
+  user = User.new(email: Faker::Internet.email, password: 'secret_password', password_confirmation: 'secret_password')
+  user.save!
+  organ = Organ.new(organ_type: rand(organ_type), city: 'Lyon', condition: 'Pas ouf', price: 20.0)
+  organ.owner_id = user.id
+  organ.save!
+  puts "Add user #{user.email}"
+end
+puts 'Finish'
