99a77ac86f2d088cb70ef391750dbb95bb63fdf9
Nathan Berthier <nathanberthier@outlook.fr>
2021-08-17 12:23:08 +0200

    Seed creation

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+User.destroy_all
+Organ.destroy_all
+puts 'Clear db'
+
+10.times do
+  user = User.new(email: Faker::Internet.email, password: 'secret_password', password_confirmation: 'secret_password')
+  user.save!
+  organ = Organ.new(type: 'Poumon', city: 'Lyon', condition: 'Pas ouf', price: 20.0)
+  organ.owner_id = user
+  organ.save!
+  puts "Add user #{user.name}"
+end
+puts 'Finish'
