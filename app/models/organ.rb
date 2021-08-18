9b10a0cec7d814bbe47a86ba1b3b8150792b86e8
Anne-Charlotte Morizot <annecharlotte.morizot@gmail.com>
2021-08-18 15:21:46 +0200

    ajouts picto dans les assets et cards en cours

--- app/models/organ.rb
+++ app/models/organ.rb
@@ -2,6 +2,6 @@ class Organ < ApplicationRecord
   belongs_to :owner, class_name: "User"
   has_many :bookings
 
-  TYPES = ['poumon', 'coeur', 'foie', 'pancréas', 'rein', 'burnasse']
+  TYPES = ['poumon', 'coeur', 'foie', 'cerveau', 'rein', 'burnasse']
   CITIES = ['Lyon', 'Bourg en Bresse', 'New-York', 'Guilherand-Granges', 'Mions', 'Soucieu-en-Jarrest']
 end
