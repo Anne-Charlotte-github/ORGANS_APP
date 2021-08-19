2c81ce94cfff2afc430fb8e764868b174ba90ddd
Simon Granger <nothem6@gmail.com>
2021-08-19 11:35:09 +0200

    add scope desc date

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,13 +9,17 @@ Organ.destroy_all
 User.destroy_all
 puts 'Clear db'
 
-user = User.new(username: 'Nathan', email: 'toto@gmail.com', password: '123456', password_confirmation: '123456')
+nathan = User.new(username: 'Nathan', email: 'toto@gmail.com', password: '123456', password_confirmation: '123456')
 
-user.save!
-organ = Organ.new(organ_type: Organ::TYPES.sample, city: 'Bourg en Bresse', condition: 'Pas ouf', price: 20.0)
-organ.owner_id = user.id
-organ.save!
-puts "Add user #{user.email}"
+nathan.save!
+organ = Organ.create!(
+  organ_type: Organ::TYPES.sample,
+  city: 'Bourg en Bresse',
+  condition: 'Pas ouf',
+  price: 20.0,
+  owner: nathan,
+)
+puts "Add user #{nathan.email}"
 
 20.times do
   user = User.new(username: Faker::Internet.username, email: Faker::Internet.email, password: 'secret_password', password_confirmation: 'secret_password')
@@ -25,4 +29,9 @@ puts "Add user #{user.email}"
   organ.save!
   puts "Add user #{user.email}"
 end
+
+Booking.create(organ: Organ.last, customer_id: nathan.id, renting_at: Date.today)
+Booking.create(organ: Organ.last, customer_id: nathan.id, renting_at: (Date.today - 2) )
+
+
 puts 'Finish'
