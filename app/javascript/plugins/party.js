fb63be1271a21b660ae06d1c8822940d4016746b
Anne-Charlotte Morizot <annecharlotte.morizot@gmail.com>
2021-08-20 13:48:41 +0200

    not heart, only confetti, sorry

--- app/javascript/plugins/party.js
+++ app/javascript/plugins/party.js
@@ -1,8 +1,12 @@
 import party from 'party-js';
+import { range } from 'party-js/lib/systems/variation';
 
 const initParty = () => {
   const confettiHTML = document.getElementById("confetti");
-  if (confettiHTML) {party.confetti(confettiHTML)};
+  if (confettiHTML) {party.confetti(confettiHTML, {
+    count: range(100, 151),
+  })
+  };
 };
 
 export {initParty}
